89e4d1873b75987933bd6082d495964b521d5492
Kristian Schjelderup <kschjeld@mac.com>
2023-04-22 15:48:39 +0200

    feat: initial version

---
+++ api/v1/networktest_types.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NetworktestSpec defines the desired state of Networktest
+type NetworktestSpec struct {
+
+	// +kubebuilder:validation:Enum:={"dial","tcp","http"}
+	Type     string `json:"type"`
+	Address  string `json:"address"`
+	Interval string `json:"interval"` // Default 1h
+}
+
+func (s NetworktestSpec) GetInterval() string {
+	if s.Interval == "" {
+		return "1h"
+	}
+	return s.Interval
+}
+
+// NetworktestStatus defines the observed state of Networktest
+type NetworktestStatus struct {
+	Accepted bool `json:"accepted,omitempty"`
+
+	// +optional
+	LastRun *metav1.Time `json:"lastRun"`
+
+	// +optional
+	NextRun *metav1.Time `json:"nextRun"`
+
+	// +optional
+	LastResult *string `json:"lastResult"`
+
+	// +optional
+	Message *string `json:"message"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Networktest is the Schema for the networktests API
+type Networktest struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NetworktestSpec   `json:"spec,omitempty"`
+	Status NetworktestStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// NetworktestList contains a list of Networktest
+type NetworktestList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Networktest `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Networktest{}, &NetworktestList{})
+}
---
+++ controllers/networktest_controller.go
@@ -0,0 +1,193 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"net/http"
+	"time"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	edgeworksnov1 "edgeworks.no/networktester/api/v1"
+)
+
+// NetworktestReconciler reconciles a Networktest object
+type NetworktestReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+	Tests  map[string]*edgeworksnov1.Networktest
+}
+
+//+kubebuilder:rbac:groups=edgeworks.no,resources=networktests,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=edgeworks.no,resources=networktests/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=edgeworks.no,resources=networktests/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Networktest object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
+func (r *NetworktestReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	var test edgeworksnov1.Networktest
+	if err := r.Get(ctx, req.NamespacedName, &test); err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+
+		// Error reading the object - requeue the request.
+		ctrl.Log.Error(err, "Failed to get Networktest")
+		return ctrl.Result{}, err
+	}
+
+	//ctrl.Log.Info("Got object: " + req.NamespacedName.String() + " (version: " + test.ObjectMeta.ResourceVersion + ")")
+
+	if !test.Status.Accepted {
+		// Verify and set status
+		if test.Spec.Address != "" {
+			test.Status.Accepted = true
+			if err := r.Status().Update(ctx, &test); err != nil {
+				ctrl.Log.Error(err, "Failed to update status of Networktest")
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	if test.Status.Accepted {
+		if ct, found := r.Tests[req.NamespacedName.String()]; !found {
+			r.Tests[req.NamespacedName.String()] = test.DeepCopy()
+		} else {
+			if ct.ResourceVersion != test.ResourceVersion {
+				r.Tests[req.NamespacedName.String()] = test.DeepCopy()
+			}
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *NetworktestReconciler) tester() {
+
+	for {
+		for n, t := range r.Tests {
+			ctrl.Log.Info(fmt.Sprintf("Testing %s", n))
+
+			var result testResult
+
+			switch t.Spec.Type {
+			case "http":
+				result = doHttpTest(t)
+			default:
+				ctrl.Log.Info(fmt.Sprintf("Don't know how to perform test of type %s", t.Spec.Type))
+				continue
+			}
+
+			var test edgeworksnov1.Networktest
+			if err := r.Get(context.Background(), types.NamespacedName{Namespace: t.ObjectMeta.Namespace, Name: t.ObjectMeta.Name}, &test); err == nil {
+				test.Status.LastResult = result.String()
+				test.Status.Message = &result.message
+				now := metav1.NewTime(time.Now())
+				test.Status.LastRun = &now
+
+				d, _ := time.ParseDuration(test.Spec.GetInterval())
+				next := metav1.NewTime(time.Now().Add(d))
+				test.Status.NextRun = &next
+
+				r.Status().Update(context.Background(), &test)
+			}
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+}
+
+type testResult struct {
+	success bool
+	message string
+}
+
+const (
+	success = "Success"
+	failed  = "Failed"
+)
+
+func (t testResult) String() *string {
+	var res string
+	switch t.success {
+	case true:
+		res = success
+	default:
+		res = failed
+	}
+	return &res
+}
+
+func doHttpTest(t *edgeworksnov1.Networktest) testResult {
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+
+	r, err := http.NewRequestWithContext(ctx, http.MethodGet, t.Spec.Address, nil)
+	if err != nil {
+		return testResult{
+			success: false,
+			message: err.Error(),
+		}
+	}
+
+	c := http.Client{}
+	res, err := c.Do(r)
+
+	if err != nil {
+		return testResult{
+			success: false,
+			message: err.Error(),
+		}
+	}
+
+	return testResult{
+		success: res.StatusCode == 200,
+		message: res.Status,
+	}
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *NetworktestReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	r.Tests = make(map[string]*edgeworksnov1.Networktest)
+
+	go r.tester()
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&edgeworksnov1.Networktest{}).
+		Owns(&v1.Pod{}).
+		Complete(r)
+}
